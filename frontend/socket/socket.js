d0ae6b0ffd7ffa0acbf208f672483e23e9609ad6
AbhishekYadav <abhishekharshit928@gmail.com>
2026-06-20 22:53:32 +0530

    prepare frontend for deployment

--- frontend/socket/socket.js
+++ frontend/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://whatsapp-clone-fcp2.onrender.com", {
   withCredentials: true,
   transports: ["websocket"],
 });
