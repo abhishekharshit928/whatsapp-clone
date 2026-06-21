67b5f4f99cbf5b3c5ba957d14ecf2f3a62e98eb4
AbhishekYadav <abhishekharshit928@gmail.com>
2026-06-21 11:42:39 +0530

    some ui changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,3 +12,6 @@ createRoot(document.getElementById('root')).render(
      </Provider> 
     </BrowserRouter>
 )
+
+const loader = document.getElementById('initial-loader')
+// if (loader) loader.remove()
