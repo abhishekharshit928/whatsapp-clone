5e226430ca3f757069520f4fce571ee59cd59c4d
AbhishekYadav <abhishekharshit928@gmail.com>
2026-06-25 12:44:52 +0530

    fix: re-fetch online users on userOnline event instead of adding single ID

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Chats from "../components/chats";
@@ -19,83 +19,71 @@ function Home() {
   const user = useSelector((state) => state.auth.user);
   const selectedChat = useSelector((state) => state.chat.selectedChat);
   const openSearch = useSelector((state) => state.ui.openSearch);
+  const storeLoading = useSelector((state) => state.auth.storeLoading);
 
   const dispatch = useDispatch();
-  const storeLoading = useSelector((state) => state.auth.storeLoading);
   const navigate = useNavigate();
 
-  const isLoggedIn = Boolean(user);
-
   useEffect(() => {
-    if (!storeLoading && !user) {  
+    if (!storeLoading && !user) {
       navigate("/signup");
     }
   }, [storeLoading, user, navigate]);
 
 
-  useEffect(() => {
-    if (!user?._id) return;
-
-    socket.emit("join", user._id);
-  }, [user?._id]);
-
-useEffect(() => {
-  const fetchOnlineUsers = async () => {
+  const fetchOnlineUsers = useCallback(async () => {
     try {
       const res = await api.get("/user/online");
       dispatch(setOnlineUsers(res.data.map((u) => u._id)));
     } catch (err) {
       console.log(err);
     }
-  };
-
-  const timer = setTimeout(fetchOnlineUsers, 4000);
-   const interval = setInterval(fetchOnlineUsers, 15000);
-
-  return () => {
-    clearTimeout(timer)
-    clearInterval(interval);
-  }
-}, [dispatch]);
-
+  }, [dispatch]);
 
   useEffect(() => {
-    const handleUserOnline = (userId) => dispatch(addOnlineUser(userId));
+    const timer = setTimeout(fetchOnlineUsers, 2000);
+    const interval = setInterval(fetchOnlineUsers, 10000);
+    return () => {
+      clearTimeout(timer);
+      clearInterval(interval);
+    };
+  }, [fetchOnlineUsers]);
+  useEffect(() => {
+    const handleUserOnline = () => fetchOnlineUsers();
     const handleUserOffline = (userId) => dispatch(removeOnlineuser(userId));
- 
+
     socket.on("userOnline", handleUserOnline);
     socket.on("userOffline", handleUserOffline);
- 
+
     return () => {
       socket.off("userOnline", handleUserOnline);
       socket.off("userOffline", handleUserOffline);
     };
-  }, [dispatch]);
+  }, [fetchOnlineUsers, dispatch]);
+
+  if (storeLoading) return null;
 
   return (
     <div className="flex overflow-hidden" style={{ height: "100dvh" }}>
       <div
         className={`
-        bg-[#111827]
-        border-r border-[#243044]
-        w-full
-        min-[760px]:w-[35%]
-        ${selectedChat ? "hidden min-[760px]:block" : "block"}
-      `}
+          bg-[#111827]
+          border-r border-[#243044]
+          w-full
+          min-[760px]:w-[35%]
+          ${selectedChat ? "hidden min-[760px]:block" : "block"}
+        `}
       >
         <div className="min-[760px]:hidden flex items-center justify-between px-4 py-3 bg-[#0F172A] border-b border-[#243044]">
           <div className="flex items-center gap-3">
             <SideBar />
-
             <span className="text-white font-semibold text-lg">NovaChat</span>
           </div>
-
           <MobileSearch />
         </div>
 
         <div className="hidden min-[760px]:flex items-center gap-3 p-4">
           <SideBar />
-
           <LaptopSearch />
         </div>
 
@@ -110,12 +98,12 @@ useEffect(() => {
 
       <div
         className={`
-        bg-[#0B1220]
-        flex-1
-        h-full
-        text-gray-400
-        ${selectedChat ? "block" : "hidden min-[760px]:block"}
-      `}
+          bg-[#0B1220]
+          flex-1
+          h-full
+          text-gray-400
+          ${selectedChat ? "block" : "hidden min-[760px]:block"}
+        `}
       >
         <ChatBox />
       </div>
