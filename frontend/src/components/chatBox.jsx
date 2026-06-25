1310fa7e762300ba41cdbea69cff3559a113638a
AbhishekYadav <abhishekharshit928@gmail.com>
2026-06-25 16:46:35 +0530

    fix: min-w-0 w-0 on chat container to prevent AI message overflow

--- frontend/src/components/chatBox.jsx
+++ frontend/src/components/chatBox.jsx
@@ -30,7 +30,7 @@ const chatBox = () =>{
       <div className="flex flex-col h-full">
         <ChatHead></ChatHead>
 
-        <div className="flex-1 overflow-y-auto ultra-thin-scroll overflow-x-hidden">
+        <div className="flex-1 overflow-y-auto ultra-thin-scroll overflow-x-hidden min-w-0 w-0">
           <ChatWindow></ChatWindow>
         </div>
 
